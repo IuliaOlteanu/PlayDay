52e0ee2aac86e77fc64b993e1244f88ffce65895
IuliaOlteanu <iulia.olteanu@stud.acs.upb.ro>
2025-04-26 16:38:46 +0300

    Template FieldsPage & GamesPage

---
+++ src/pages/FieldsPage.tsx
@@ -0,0 +1,10 @@
+function FieldsPage() {
+  return (
+    <div>
+      <h1>Fields Page</h1>
+      <p>This is the fields page.</p>
+    </div>
+  );
+}
+
+export default FieldsPage;
---
+++ src/pages/GamesPage.tsx
@@ -0,0 +1,9 @@
+function GamesPage() {
+  return (
+    <div>
+      <h1>Games</h1>
+      <p>Welcome to the Games Page!</p>
+    </div>
+  );
+}
+export default GamesPage;
